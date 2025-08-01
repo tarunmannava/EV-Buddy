2d3717511110a73a9e903ffc2512eff3f6640c5a
Tarun Mannava <tarun.mannava@gmail.com>
2025-08-01 05:47:04 -0400

    added EVBuddyApp

--- app/src/main/java/com/example/evbuddy/MainActivity.kt
+++ app/src/main/java/com/example/evbuddy/MainActivity.kt
@@ -19,29 +19,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EVBuddyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    EVBuddyApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun EVBuddyMainAppPreview() {
     EVBuddyTheme {
-        Greeting("Android")
+        EVBuddyApp()
     }
 }
+
